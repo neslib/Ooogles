1ad8698994f67dc2831c5ef2adae1fed249eecb6
Erik van Bilsen <erik@grijjy.com>
2017-07-05 12:42:20 -0700

    Added documentation

--- DocSource/BuildHtml.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set FORMAT=Html
-set SEARCH=
-call Build.bat
-rem ..\Doc\Html\index.html
-rem pause
--- DocSource/BuildHtmlHelp.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set FORMAT=HtmlHelp
-set HHC="%ProgramFiles(x86)%\HTML Help Workshop\hhc"
-set SEARCH=
-call Build.bat
-%HHC% ..\Doc\HtmlHelp\Ooogles.hhp
-copy ..\Doc\HtmlHelp\Ooogles.chm ..\Doc\ /y
-..\Doc\Ooogles.chm
-pause
